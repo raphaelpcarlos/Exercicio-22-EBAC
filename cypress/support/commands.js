f55313874db6f6f97306b5be8cfefb7902cf41ee
raphaelpcarlos <raphaelpereiracarlos@gmail.com>
2023-04-01 16:46:29 -0300

    first commit

---
+++ cypress/support/commands.js
@@ -0,0 +1,16 @@
+Cypress.Commands.add("logOff", () => {
+    cy.get('.topbar-inner > :nth-child(1) > .list-inline > :nth-child(2) > a').click();
+    cy.get('.icon-user-unfollow').should('be.visible');
+})
+
+Cypress.Commands.add("login", ({email, pass}) => {
+    cy.visit('/minha-conta/')
+    cy.get('#username').clear();
+    cy.get('#username').type(email);
+    cy.get('#password').clear();
+    cy.get('#password').type(pass);
+    cy.get('#rememberme').check();
+    cy.get('.woocommerce-form > .button').click();
+    cy.get('.page-title').should('have.text', 'Minha conta');
+})
+
